4494beffe89247adb45a117278789cc2ef34fa37
Isabella1245 <99446329+Isabella1245@users.noreply.github.com>
2023-02-08 20:01:30 -0500

    constants changed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,8 +3,8 @@ package frc.robot;
 public interface Constants{
     
     //dimensions of robot in inches
-    public final double L = 12;
-    public final double W = 12;
+    public final double L = 33.125;
+    public final double W = 18.5;
 
     //PIDF variables 
     //the difference between PID and PIDF:
@@ -20,22 +20,22 @@ public interface Constants{
 
     //Turn motor CAN ID
     // *** im not quite sure what the IDs are for ***
-    public final int FLTid = 1;
-    public final int FRTid = 2;
-    public final int BRTid = 3;
-    public final int BLTid = 4;
+    public final int FLTid = 2;
+    public final int FRTid = 3;
+    public final int BRTid = 5;
+    public final int BLTid = 7;
 
     //IDs for drive motors
-    public final int FRDid = 5;
-    public final int FLDid = 6;
-    public final int BRDid = 7;
+    public final int FRDid = 4;
+    public final int FLDid = 1;
+    public final int BRDid = 6;
     public final int BLDid = 8;
 
     // Analog Encoder ID (i think they are refering to turning encoders)
-    public final int FRTencoderID = 9;
-    public final int FLTencoderID = 10;
-    public final int BRTencoderID = 11;
-    public final int BLTencoderID = 12;
+    public final int FRTencoderID = 2;
+    public final int FLTencoderID = 3;
+    public final int BRTencoderID = 0;
+    public final int BLTencoderID = 1;
 
     // Offset of encoders to make them face forwards
     public final int FRTencoderOffset = 50;
